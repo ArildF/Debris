20d40db7dc0684590f632ef972929bdd1bee8716
Arild Fines <arild.fines@broadpark.no>
2021-11-04 00:21:20 +0100

    Added lateral and vertical thrust

--- Assets/Player/PlayerControl.cs
+++ Assets/Player/PlayerControl.cs
@@ -1,63 +1,89 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerControl : MonoBehaviour
+namespace Player
 {
-    public InputAction thrustAction;
-    public InputAction reverseThrustAction;
-    public InputAction lateralRollAction;
-    public InputAction medialRollAction;
-    public float forwardForce = 1_000_000;
-    public float reverseForce = 500_000;
-    public float lateralRollForce = 100_000;
-    public float medialRollForce = 100_000;
-    private Rigidbody _rigidBody;
-
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerControl : MonoBehaviour
     {
-        _rigidBody = GetComponent<Rigidbody>();
-
-    }
-
-    private void OnEnable()
-    {
-        foreach(var action in new[]{thrustAction, reverseThrustAction, lateralRollAction, medialRollAction})
-            action.Enable();
-    }
+        public InputAction thrustAction;
+        public InputAction reverseThrustAction;
+        public InputAction lateralRollAction;
+        public InputAction medialRollAction;
+        public InputAction lateralThrustAction;
+        public InputAction verticalThrustAction;
     
-    private void OnDisable()
-    {
-        foreach(var action in new[]{thrustAction, reverseThrustAction, lateralRollAction, medialRollAction})
-            action.Disable();
-    }
+        public float forwardForce = 1_000_000;
+        public float reverseForce = 500_000;
+        public float lateralRollForce = 100_000;
+        public float medialRollForce = 100_000;
+        public float lateralThrustForce = 100_000;
+        public float verticalThrustForce = 100_000;
+        private Rigidbody _rigidBody;
+        private InputAction[] _actions;
 
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        if (thrustAction.phase == InputActionPhase.Started)
+        // Start is called before the first frame update
+        void Start()
         {
-            _rigidBody.AddForce(transform.forward * forwardForce, ForceMode.Impulse); 
-        }
-        if (reverseThrustAction.phase == InputActionPhase.Started)
-        {
-            _rigidBody.AddForce(transform.forward * -reverseForce, ForceMode.Impulse); 
-        }
-
-        if (lateralRollAction.phase == InputActionPhase.Started)
-        {
-            var roll = lateralRollAction.ReadValue<float>();
-            _rigidBody.AddRelativeTorque(0, 0, -roll * lateralRollForce);
-        }
+            _rigidBody = GetComponent<Rigidbody>();
         
-        if (medialRollAction.phase == InputActionPhase.Started)
-        {
-            var roll = medialRollAction.ReadValue<float>();
-            _rigidBody.AddRelativeTorque(roll * medialRollForce, 0, 0);
-        }
-    }
 
+        }
+
+        private void OnEnable()
+        {
+            _actions = new[]
+            {
+                thrustAction, reverseThrustAction, lateralRollAction, medialRollAction, lateralThrustAction,
+                verticalThrustAction
+            }; 
+        
+            foreach(var action in _actions)
+                action.Enable();
+        }
+    
+        private void OnDisable()
+        {
+            foreach(var action in _actions)
+                action.Disable();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            if (thrustAction.phase == InputActionPhase.Started)
+            {
+                _rigidBody.AddForce(transform.forward * forwardForce, ForceMode.Impulse); 
+            }
+            if (reverseThrustAction.phase == InputActionPhase.Started)
+            {
+                _rigidBody.AddForce(transform.forward * -reverseForce, ForceMode.Impulse); 
+            }
+
+            if (lateralRollAction.phase == InputActionPhase.Started)
+            {
+                var roll = lateralRollAction.ReadValue<float>();
+                _rigidBody.AddRelativeTorque(0, 0, -roll * lateralRollForce);
+            }
+        
+            if (medialRollAction.phase == InputActionPhase.Started)
+            {
+                var roll = medialRollAction.ReadValue<float>();
+                _rigidBody.AddRelativeTorque(roll * medialRollForce, 0, 0);
+            }
+
+            if (lateralThrustAction.phase == InputActionPhase.Started)
+            {
+                var thrust = lateralThrustAction.ReadValue<float>();
+                _rigidBody.AddForce(transform.right * thrust * lateralThrustForce, ForceMode.Impulse);
+            }
+
+            if (verticalThrustAction.phase == InputActionPhase.Started)
+            {
+                var thrust = verticalThrustAction.ReadValue<float>();
+                _rigidBody.AddForce(transform.up * thrust * verticalThrustForce, ForceMode.Impulse);
+
+            }
+        }
+
+    }
 }
