7ab5a6910ee2e1411cb4606f03f53def207359fc
Arild Fines <arild.fines@broadpark.no>
2022-07-05 16:19:10 +0200

    Some clean up

--- Assets/Player/TargetTracker.cs
+++ Assets/Player/TargetTracker.cs
@@ -1,4 +1,5 @@
 using System;
+using JetBrains.Annotations;
 using Player.UI;
 using UniDi;
 using UnityEngine;
@@ -35,20 +36,13 @@ namespace Player
             {
                 Vector3 point = camera.WorldToScreenPoint(_targetInfo.Target.position);
                 Vector3 original = point;
-                print($"Unprocessed reticle screen point {point}");
 
-                // if (point.z < 0)
-                // {
-                //     point *= -1;
-                // }
                 point.x = Mathf.Clamp(point.x, 0, camera.pixelWidth);
                 point.y = Mathf.Clamp(point.y, 0, camera.pixelHeight);
 
-                var calculated = CalculateFrustumIntersection();
-
                 if (point != original || point.z < 0)
                 {
-                    point = calculated;
+                    point = CalculateScreenEdgeIntersection();
                     if (!_reticleCircle.fill)
                     {
                         _reticleCircle.fill = true;
@@ -61,86 +55,66 @@ namespace Player
                     _reticleCircle.SetVerticesDirty();
                 }
 
-                // var start = _playerViewInfo.ShipTransform.position + _playerViewInfo.ShipTransform.forward * 50;
-                // // Debug.DrawLine(start, _targetInfo.Target.position, Color.green);
-                // camera.CalculateFrustumCorners();
-                print($"Processed reticle screen point {point}");
                 _reticleCircle.transform.position = (Vector2)point;
             }
 
         }
 
-        private Vector3 CalculateFrustumIntersection()
+        private Vector3 CalculateScreenEdgeIntersection()
         {
             var player = _playerViewInfo.ShipTransform;
-            var colors = new[] { Color.blue, Color.green, Color.magenta, Color.red };
             var start = player.position + player.forward * 50;
 
-            // for (int i = 0; i < frustumCorners.Length; i++)
-            // {
-            //     // Debug.DrawLine(start, frustumCorners[i], colors[i]);
-            // }
-            
-            Debug.DrawLine(start, _targetInfo.Target.position, Color.red);
-
-            var direction = (_targetInfo.Target.position - start);
+            var direction = _targetInfo.Target.position - start;
             var projectedDirection = Quaternion.Inverse(player.rotation) * Vector3.ProjectOnPlane(direction, _playerViewInfo.ShipTransform.forward);
 
-            print($"Projected direction: {projectedDirection}");
-            Debug.DrawRay(start, projectedDirection * 1000, Color.magenta);
-
+            var height = camera.pixelHeight;
+            var width = camera.pixelWidth;
             var planes = new[]
             {
                 new Plane(Vector3.right, new Vector3(0, 0, 0)),
-                new Plane(Vector3.down, new Vector3(0, camera.pixelHeight, 0)),
-                new Plane(Vector3.left, new Vector3(camera.pixelWidth, camera.pixelHeight, 0)),
-                new Plane(Vector3.up, new Vector3(camera.pixelWidth, 0, 0)),
+                new Plane(Vector3.down, new Vector3(0, height, 0)),
+                new Plane(Vector3.left, new Vector3(width, height, 0)),
+                new Plane(Vector3.up, new Vector3(width, 0, 0)),
             };
 
-            var centerScreen = new Vector3(camera.pixelWidth / 2, camera.pixelHeight / 2, 0);
+            var centerScreen = new Vector3(width / 2f, height / 2f, 0);
             projectedDirection.z = 0;
             var ray = new Ray(centerScreen, projectedDirection);
-            print($"Ray: {ray}");
 
             Vector3 closest = Vector3.zero;
-            for (int i = 0; i < planes.Length; i++)
+            for (var i = 0; i < planes.Length; i++)
             {
                 var plane = planes[i];
-                
-                // DebugDrawPlane(plane, colors[i]);
                 
                 if (plane.Raycast(ray, out float enter))
                 {
                     var intersect = ray.GetPoint(enter);
                     
-                    print($"Intersected with frustum plane {i} at ss {intersect}");
-                    // Debug.DrawLine(start, intersect, Color.yellow);
-
                     if (closest == Vector3.zero || (intersect - centerScreen).magnitude < closest.magnitude)
                     {
                         closest = intersect;
                     }
-
                 }
             }
 
             if (closest != Vector3.zero)
             {
-                print($"Closest: {closest}");
                 return closest;
             }
             throw new Exception("Really should not happen.");
 
         }
 
+        [UsedImplicitly]
         private void DebugDrawPlane(Plane plane, Color color)
         {
             const int planeSize = 100;
             var player = _playerViewInfo.ShipTransform;
             const int halfPlane = planeSize / 2;
-            var origin = player.position + player.forward * halfPlane - plane.normal.normalized * plane.distance;
-
             var forward = player.forward;
+            var origin = player.position + forward * halfPlane - plane.normal.normalized * plane.distance;
+
             var perp = Vector3.Cross(forward, plane.normal);
             Debug.DrawLine(origin - perp * halfPlane - forward * halfPlane, 
                 origin - perp * halfPlane + forward * halfPlane, color);
