28bd030c4035d0164fb7060ed9494537eb1d7ec3
Arild Fines <arild.fines@broadpark.no>
2022-07-06 17:44:45 +0200

    Inject the camera through UniDI

--- Assets/Player/TargetTracker.cs
+++ Assets/Player/TargetTracker.cs
@@ -9,19 +9,17 @@ namespace Player
     [RequireComponent(typeof(ReticleCircle))]
     public class TargetTracker : MonoBehaviour
     {
+        [Inject]
         private TargetInfo _targetInfo;
         private ReticleCircle _reticleCircle;
 
-        public new Camera camera;
+        [Inject]
+        private Camera _camera;
         public Transform manualTarget;
+        
+        [Inject]
         private PlayerViewInfo _playerViewInfo;
 
-        [Inject]
-        public void Init(TargetInfo targetInfo, PlayerViewInfo playerViewInfo)
-        {
-            _targetInfo = targetInfo;
-            _playerViewInfo = playerViewInfo;
-        }
 
         private void Start()
         {
@@ -32,13 +30,13 @@ namespace Player
         private void Update()
         {
             _reticleCircle.enabled = _targetInfo.Target != null;
-            if (_reticleCircle.enabled)
+            if (_targetInfo.Target != null)
             {
-                Vector3 point = camera.WorldToScreenPoint(_targetInfo.Target.position);
+                Vector3 point = _camera.WorldToScreenPoint(_targetInfo.Target.position);
                 Vector3 original = point;
 
-                point.x = Mathf.Clamp(point.x, 0, camera.pixelWidth);
-                point.y = Mathf.Clamp(point.y, 0, camera.pixelHeight);
+                point.x = Mathf.Clamp(point.x, 0, _camera.pixelWidth);
+                point.y = Mathf.Clamp(point.y, 0, _camera.pixelHeight);
 
                 if (point != original || point.z < 0)
                 {
@@ -68,8 +66,8 @@ namespace Player
             var direction = _targetInfo.Target.position - start;
             var projectedDirection = Quaternion.Inverse(player.rotation) * Vector3.ProjectOnPlane(direction, _playerViewInfo.ShipTransform.forward);
 
-            var height = camera.pixelHeight;
-            var width = camera.pixelWidth;
+            var height = _camera.pixelHeight;
+            var width = _camera.pixelWidth;
             var planes = new[]
             {
                 new Plane(Vector3.right, new Vector3(0, 0, 0)),
--- Assets/UniDiInstaller.cs
+++ Assets/UniDiInstaller.cs
@@ -1,14 +1,17 @@
 using HUD;
 using Player;
 using UniDi;
+using UnityEngine;
 
 public class UniDiInstaller : MonoInstaller
 {
+    public Camera mainCamera;
     public override void InstallBindings()
     {
         Container.Bind<HudInfo>().AsSingle();
         Container.Bind<ThrustInfo>().AsSingle();
         Container.Bind<PlayerViewInfo>().AsSingle();
         Container.Bind<TargetInfo>().AsSingle();
+        Container.Bind<Camera>().FromInstance(mainCamera);
     }
 }
